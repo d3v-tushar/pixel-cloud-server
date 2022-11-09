66eb1c9b3ae44d801d1d4f4dfcd9d88a810e5905
d3v-tushar <d3v.tushar@gmail.com>
2022-11-09 22:18:14 +0600

    Get all Packages Data From DB

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config
+require('dotenv').config();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 //MongoDB
-const uri = "mongodb+srv://<username>:<password>@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async() =>{
     try{
     const packagesCollection = client.db("pixelCloud").collection("packages");
+    app.get('/packages', async(req, res) =>{
+        const query = {};
+        const cursor = packagesCollection.find(query);
+        const packages = await cursor.toArray();
+        res.send(packages);
+    })
     }
     finally{
         // client.close();
