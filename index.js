80d9cc4cec5299d5ff8ec9ccce04ddcd9c023450
d3v-tushar <d3v.tushar@gmail.com>
2022-11-10 07:39:05 +0600

    Delete Function Added For Reviews

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ const run = async() =>{
         res.send(result);
     });
 
-    app.delete('/reviews/id', async(req, res) =>{
+    app.delete('/reviews/:id', async(req, res) =>{
         const id = req.params.id;
-        const query = {packageId: id};
+        const query = {_id: ObjectId(id)};
         const result = await reviewsCollection.deleteOne(query);
         res.send(result);
     });
