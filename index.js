162ad9add9dd4f3114cf651aac08277f9db3ba63
d3v-tushar <d3v.tushar@gmail.com>
2022-11-22 06:38:56 +0600

    JWT Token Added & Vercel Deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectID, ObjectId } = require('bson');
 const app = express();
@@ -13,15 +14,40 @@ app.use(express.json());
 //MongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//Verify JWT
+const verifyJwt = (req, res, next) =>{
+    const authToken = req.headers.authorization;
+    if(!authToken){
+        return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authToken.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY, (error, decoded) =>{
+        if(error){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 const run = async() =>{
     try{
     const packagesCollection = client.db("pixelCloud").collection("packages");
     const reviewsCollection = client.db("pixelCloud").collection("reviews");
+
+    //JWT Token
+    app.post('/jwt', (req, res) =>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.SECRET_KEY, {expiresIn: '1hr'});
+        res.send({token});
+    })
+
+
     app.get('/packages', async(req, res) =>{
         const query = {};
         const options = {
             sort: { publishTime: -1 },
-            projection: { _id: 0, publishTime: 1},
           };
         const cursor = packagesCollection.find(query, options);
         const packages = await cursor.toArray();
@@ -32,8 +58,7 @@ const run = async() =>{
     app.get('/packages/home', async(req, res) =>{
         const query = {};
         const options = {
-            sort: { publishTime: -1 },
-            projection: { _id: 0, publishTime: 1},
+            sort: { publishTime: -1 }
           };
         const cursor = packagesCollection.find(query, options).limit(3);
         const packages = await cursor.toArray();
@@ -54,6 +79,7 @@ const run = async() =>{
         res.send(result);
     });
 
+    //Display Reviews for each Package
     app.get('/reviews', async(req, res) =>{
         let query = {};
         if(req.query.packageId){
@@ -67,7 +93,11 @@ const run = async() =>{
     });
 
     //API For My Reviews Page
-    app.get('/myreviews', async(req, res) =>{
+    app.get('/myreviews', verifyJwt, async(req, res) =>{
+        const decoded = req.decoded;
+        if(decoded.email !== req.query.email){
+            return res.send({message: 'Access Denied'})
+        }
         let query = {};
         if(req.query.email){
             query = {
